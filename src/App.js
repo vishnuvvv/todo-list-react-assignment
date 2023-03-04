1af7a764177cff6b2a0eff3302e2ca73f05aa17b
VISHNU V <vishnusathyanathan@gmail.com>
2023-03-05 01:09:50 +0530

    modified

--- src/App.js
+++ src/App.js
@@ -5,15 +5,11 @@ import UserDetails from "./components/userDetails/UserDetails";
 
 const App = () => {
   const [selectedTodo, setSelectedTodo] = useState(null);
- 
 
-  /* here we have defined handle view user function to-
-   view the user details and we have passed- as props into the todolist table component */
   const handleViewUser = (todo) => {
     setSelectedTodo(todo);
   };
 
- 
   return (
     <div className="app-container">
       <section className="todoListTable">
--- src/components/userDetails/UserDetails.js
+++ src/components/userDetails/UserDetails.js
@@ -2,33 +2,54 @@ import { useEffect, useState } from "react";
 import { fetchUser } from "../../api-helpers/api-helper";
 import "./UserDetails.css";
 
-const UserDetails = ({todo}) => {
-  const [user , setUser] = useState(" ")
+const UserDetails = ({ todo }) => {
+  const [user, setUser] = useState(" ");
 
-  useEffect(() =>{
-    if(todo){
-      fetchUser(todo.userId).then((data)=>{
-        setUser(data)
-      }).catch((err)=>console.log(err))
+  useEffect(() => {
+    if (todo) {
+      fetchUser(todo.userId)
+        .then((data) => {
+          setUser(data);
+        })
+        .catch((err) => console.log(err));
     }
-  },[todo])
+  }, [todo]);
   return (
-    <div className="userDetailsBox">
-      <div>
-        <div>ToDo ID  :{"  "}{todo.id}</div>
-      </div>
-      <div>
-        <div>ToDo Title  :{"  "}{todo.title}</div>
-      </div>
-      <div>
-        <div>User Id  :{"  "}{todo.userId}</div>
-      </div>
-      <div>
-        <div>Name  :{"  "}{user.name}</div>
-      </div>
-      <div>
-        <div>Email  :{"  "}{user.email}</div>
-      </div>
+    <div>
+      {user && (
+        <div className="userDetailsBox">
+          <div>
+            <div>
+              ToDo ID :{"  "}
+              {todo.id}
+            </div>
+          </div>
+          <div>
+            <div>
+              ToDo Title :{"  "}
+              {todo.title}
+            </div>
+          </div>
+          <div>
+            <div>
+              User Id :{"  "}
+              {todo.userId}
+            </div>
+          </div>
+          <div>
+            <div>
+              Name :{"  "}
+              {user.name}
+            </div>
+          </div>
+          <div>
+            <div>
+              Email :{"  "}
+              {user.email}
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
